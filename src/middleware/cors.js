663b54632eba0567ad4670e4aec1ef8dd88cd91e
FranciscoConcha <Francisco.concha.urquieta@gmail.com>
2025-12-07 19:06:38 -0300

    ci: feat(cors) Cors arregladas

--- src/middleware/cors.js
+++ src/middleware/cors.js
@@ -1,8 +1,9 @@
 import cors from 'cors'
 const corsOptions = {
   origin: ['http://localhost:5173',
-    'insightflow-frontend-39727.web.app',
-    'insightflow-frontend-39727.firebaseapp.com'
+    'https://insightflow-frontend-39727.web.app',
+    'https://insightflow-frontend-39727.firebaseapp.com',
+    'https://user-services-13hx.onrender.com'
   ], // agrega más orígenes si es necesario
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization'],
