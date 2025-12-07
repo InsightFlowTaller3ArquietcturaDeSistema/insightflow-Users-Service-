4de0cae6a4a552dc84e5df9c7d41c4bf45bfadd7
FranciscoConcha <Francisco.concha.urquieta@gmail.com>
2025-12-07 03:16:32 -0300

    ci: feat(cors): se agrego las cors para el frontend

--- src/middleware/cors.js
+++ src/middleware/cors.js
@@ -1,6 +1,8 @@
 import cors from 'cors'
 const corsOptions = {
-  origin: ['http://localhost:5173'
+  origin: ['http://localhost:5173',
+    'insightflow-frontend-39727.web.app',
+    'insightflow-frontend-39727.firebaseapp.com'
   ], // agrega más orígenes si es necesario
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization'],
